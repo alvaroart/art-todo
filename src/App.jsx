8735c14e5a70f70c0805b9b362b147d68a0b2572
alvaroart <alvaro.rosa1985@gmail.com>
2023-05-26 19:23:07 +0100

    Filter component Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
 
 import './App.css'
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,6 +29,8 @@ function App() {
     },
   ]);
 
+  const [search, setSearch] = useState("");
+
 const addTodo = (text, category) => {
   const newTodos = [
     ...todos,
@@ -47,13 +51,33 @@ const removeTodo = (id) => {
     todo.id !== id ? todo : null
     );
     setTodos(filteredTodos);
-}
+};
+
+const completeTodo = (id) => {
+  const newTodos = [...todos];
+  newTodos.map((todo) => 
+  todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
+  );
+  setTodos(newTodos);
+};
+
   return (
     <div className='app'>
       <h1>Lista de Tarefas</h1> 
+      <Search search={search} setSearch={setSearch} />
+      <Filter></Filter>
       <div className='todo-list'>
-        {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} />
+        {todos
+          .filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((todo) => (
+            <Todo
+            key={todo.id} 
+            todo={todo} 
+            removeTodo={removeTodo} 
+            completeTodo={completeTodo} 
+            />
         ))}
       </div>
       <TodoForm addTodo={addTodo} />
